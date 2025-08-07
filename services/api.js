6230484ba8cab4c889d963aa8ca1775b8e9bd129
ThaiChuT9 <iamtk4i@gmail.com>
2025-08-07 18:33:36 +0700

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"; 
 import { getAuthHeaders } from "../utils/auth";
 
-console.log('API URL:', process.env.NEXT_PUBLIC_API_URL); // For debugging
+// console.log('API URL:', process.env.NEXT_PUBLIC_API_URL); // For debugging
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -22,23 +22,23 @@ api.interceptors.request.use(config => {
     return config;
 });
 
-// Thêm interceptor để log requests (để debug)
-api.interceptors.request.use(request => {
-    console.log('Starting Request:', request.url);
-    console.log('Request Config:', request.baseURL);
-    return request;
-});
+// // Thêm interceptor để log requests (để debug)
+// api.interceptors.request.use(request => {
+//     console.log('Starting Request:', request.url);
+//     console.log('Request Config:', request.baseURL);
+//     return request;
+// });
 
-// Thêm interceptor để log responses (để debug)
-api.interceptors.response.use(
-    response => {
-        console.log('Response:', response);
-        return response;
-    },
-    error => {
-        console.error('Response Error:', error);
-        return Promise.reject(error);
-    }
-);
+// // Thêm interceptor để log responses (để debug)
+// api.interceptors.response.use(
+//     response => {
+//         console.log('Response:', response);
+//         return response;
+//     },
+//     error => {
+//         console.error('Response Error:', error);
+//         return Promise.reject(error);
+//     }
+// );
 
 export default api;
